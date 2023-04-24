c37e5bd015df49be723e6f39fc4bfbbdf46402fd
MujahidAkberAli <mujahidakberali@gmail.com>
2023-04-24 22:41:09 +0500

    Add project files.

---
+++ Appium_NotePad_Automation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Appium_NotePad_Automation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Appium_NotePad_Automation")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b1597e5-fd10-4b4d-96db-597716fc74e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Appium_NotePad_Automation/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace Appium_NotePad_Automation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NotePad_Automation()
+        {
+            System.Diagnostics.Process.Start
+               (@"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe");
+
+            AppiumOptions options = new AppiumOptions();
+            options.AddAdditionalCapability("app", @"C:\Windows\notepad.exe");
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+            var driver = new WindowsDriver<WindowsElement>
+                (new Uri("http://127.0.0.1:4723"), options);
+
+            driver.FindElementByName("Help").Click();
+            driver.FindElementByName("About Notepad").Click();
+            Assert.AreEqual("Version 21H2 (OS Build 19044.2846)", driver.FindElementByAccessibilityId("13579").Text);
+            driver.FindElementByName("OK").Click();
+
+            driver.FindElementByClassName("Edit").SendKeys(DateTime.Now.ToString());
+            driver.FindElementByClassName("Edit").Clear();
+
+            driver.FindElementByName("Edit").Click();
+            driver.FindElementByAccessibilityId("26").Click();
+            Assert.IsNotNull(driver.FindElementByClassName("Edit"));
+            driver.FindElementByClassName("Edit").Clear();
+
+            string data = DateTime.Now.ToString() + " Amir Imam";
+
+            driver.FindElementByClassName("Edit").SendKeys(data);
+            driver.FindElementByName("File").Click();
+            driver.FindElementByAccessibilityId("3").Click();
+            Thread.Sleep(1000);
+
+            driver.FindElementByAccessibilityId("FileNameControlHost").SendKeys("C:\\" + DateTime.Now.ToString("yyyymmdd hmmtt") + "_AmirImam.txt");
+            driver.FindElementByAccessibilityId("1").Click();
+
+            Thread.Sleep(1000);
+            driver.CloseApp();
+        }
+    }
+}
